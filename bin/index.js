42a17cf6c2be425ddfb5fbda600f3b1926f44f21
Winford <winford@Huberts-MacBook-Pro.local>
2020-06-12 19:57:47 -0400

    added service debug

--- bin/index.js
+++ bin/index.js
@@ -2,15 +2,24 @@
 
 const program = require('commander');
 
-const create = require('../lib/create');
+const create = require('../lib/create/create');
+const debug = require('../lib/debug/podDebug');
 
 console.log();
 program
   .command('create')
   .alias('c')
   .description('Create Kubernetes Resources')
-  .action(function () {
+  .action(() => {
     create.questions();
+  });
+
+program
+  .command('debug')
+  .alias('d')
+  .description('Debug Kubernetes Resources')
+  .action(() => {
+    debug.debugPodQ();
   });
 
 program.parse(process.argv);
